0601d5a753a72edf6c2d6c33cb85318ad8ea94b3
Aradhya <aradhya@my.yorku.ca>
2024-04-22 22:14:19 -0400

    Streamlit

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ import streamlit as st
 load_dotenv()
 
 st.set_page_config(page_title= "DBChat", page_icon=":speech_balloon:")
+
+st.title("DBChat")
+
+with st.sidebar:
+    st.subheader("Settings")
+    st.write("This is a simple chat app with SQL")
+    
+    st.text_input("host", value="localhost")
+    st.text_input("Port", value="3306")
+    st.text_input("User", value="root")
+    st.text_input("Password", value="admin" type="password")
+    st.text_input("Database", value="chinook")
