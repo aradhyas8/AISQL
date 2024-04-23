5ed653d30c9591dea7e719327f8d4d693ca2a75c
Aradhya <aradhya@my.yorku.ca>
2024-04-22 23:19:12 -0400

    Streamlit

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-
+import os
 import urllib.parse
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_community.utilities import SQLDatabase
 
-def init_database(user, password, host, port, database):
-    password = urllib.parse.quote(password)
+load_dotenv()  # This loads the environment variables from the .env file.
+
+def init_database():
+    user = os.getenv("DB_USER")
+    password = urllib.parse.quote(os.getenv("DB_PASSWORD"))  # URL-encode the password
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    database = os.getenv("DB_DATABASE")
     db_uri = f"postgresql://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
 
-load_dotenv()
-
 st.set_page_config(page_title="DBChat", page_icon=":speech_balloon:")
-
 st.title("DBChat")
 
 with st.sidebar:
     st.subheader("Settings")
     st.write("This is a simple chat app with SQL")
-    
-    st.text_input("host", value="aws-0-ca-central-1.pooler.supabase.com", key="host")
-    st.text_input("Port", value="5432", key="port")
-    st.text_input("User", value="postgres.yphplsewskzrkbhrmgay", key="user")
-    st.text_input("Password", value="Ethanyork@1234", type="password", key="password")
-    st.text_input("Database", value="postgres", key="database")
-    
+
     if st.button("Connect"):
         with st.spinner("Connecting to the database"):
-            db = init_database(
-                st.session_state["user"],
-                st.session_state["password"],
-                st.session_state["host"],
-                st.session_state["port"],
-                st.session_state["database"]
-            )
-            
+            db = init_database()
             st.session_state.db = db
             st.success("Connected to the database")
-    
+
 st.chat_input("Type a message")
